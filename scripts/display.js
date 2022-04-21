9a6a02ff0b13212828cd0a3dad357f27ad06d8d6
AnthonyLaneJr <101611103+AnthonyLaneJr@users.noreply.github.com>
2022-04-20 19:58:02 -0700

    Classwork Class3
    Class work for class #3

---
+++ scripts/display.js
@@ -0,0 +1,48 @@
+//display pets in list items
+
+//  Inneficient way
+//
+// function displayPet(){
+//      //cerate text
+//     let petsDiv = document.getElementById(`pets`);
+//     petsDiv.innerHTML=``;
+//     for(let i=0;i<petSalon.pets.length;i++){
+//         let pet = petSalon.pets[i];
+//         let li = document.createElement("li");  //creates li tag
+//         let text = document.createTextNode(pet.name)
+//     li.appendChild(text);
+//     petsDiv.appendChild(li);
+// }}
+
+
+//efficient way
+
+function displayPet(){
+    //cerate text
+   let petsDiv = document.getElementById(`pets`); //assigning a div to this let (div is on HTML)
+   let tmp=``;
+   for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i]; //get each pet with loop
+        tmp+=`<li>${pet.name} --- ${pet.age}</li>`;} // += adds the previous itterations of loop into tmp variable value
+    document.getElementById(`pets`).innerHTML=tmp
+}
+
+//dispaly pets in cards
+function displayCards(){
+    let tmp=``;
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+        tmp+=`
+        <div class="col">
+                <h4>${pet.name}</h4>
+                <p>${pet.age}</p>
+                <p>${pet.gender}</p>
+                <p>${pet.breed}</p>
+                <p>${pet.phone}</p>
+        </div>`;
+    }
+    document.getElementById(`pets`).innerHTML=tmp;
+}
+
+
+//display pets in table
--- scripts/register.js
+++ scripts/register.js
@@ -9,17 +9,7 @@ let petSalon= {
     hours:{
         open:`9:00 a.m.`,
         close:`8:00 p.m.`},
-    pets:[ //for hw minimum of 4 pets- display on console and html
-    //     {
-    //         //obj 1
-    //     },
-    //     {
-    //         //obj 2
-    //     },
-    //     {
-    //         //obj 3
-    //     }
-     ]
+    pets:[]
 }
 function Pet(name,age,gender,breed,service,ownerName,contactPhone){
     this.name=name;
@@ -39,15 +29,38 @@ let inputService=document.getElementById("selService");
 let inputOwner=document.getElementById("txtOwner");
 let inputPhone=document.getElementById("tel");
 
+function isValid(aPet){    //returns false when the pet is not valid, returns true when the pet is valid (all sections filled)
+    let valid=true;
+    if(aPet.name.length==0){
+        //if the name has less than 1 character, it will mean that aPet will be defined as false or inavlid.
+        valid=false;
+        console.error(`Invalid name`);
+        alert(`Invalid name`);
+    }
+    if(aPet.service.length==0){
+        valid=false;
+        console.error(`Invalid service`);
+        alert(`Invalid service`);
+    }
+    if(aPet.phone.length==0){
+        valid=false;
+        console.error(`Invalid phone`);
+        alert(`Invalid phone`);
+    }
+    return valid;
+}
+
 function register(){
     //create pet
     let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value,);
+    if(isValid(thePet)){ //ensure that the isValid function is true before publishing to array 
     console.log(thePet);
     //push into array
     petSalon.pets.push(thePet);
     //clear the input
     clearInputs();
-    console.log(petSalon.pets)
+    console.log(petSalon.pets);
+    displayCards();}
 }
 
 function clearInputs(){
@@ -71,10 +84,13 @@ function displaySalonInfo(){
 }
 
 function displayPetNames(){
+    for(let i=0;i<petSalon.pets.length;i++){
+            console.log(petSalon.pets[i][`name`])}
 }
 
 function displayNumberOfPets(){
     alert(`Number of pets registered in system: ${petSalon.pets.length}`)
 }
+displayCards();
 
 document.getElementById(`footer`).innerHTML=(`<p>${petSalon[`name`]}</p><p>${petSalon.address[`number`]}  ${petSalon.address[`street`]}  ${petSalon.address[`city`]},   ${petSalon.address[`state`]},  ${petSalon.address[`zip`]}</p><p>Open: ${petSalon.hours[`open`]} - ${petSalon.hours[`close`]}, Mon-Sat</p>`)
